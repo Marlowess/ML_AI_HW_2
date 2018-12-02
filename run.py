1a261b40f2996a0882bcbb8f8ce1ccc5d3ffad39
Stefano Brilli <s249914@studenti.polito.it>
2018-12-02 19:29:45 +0100

    Code improved

--- run.py
+++ run.py
@@ -2,65 +2,95 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn import datasets, svm
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
 
 # This function load the iris dataset in memory and gets just the first two dimensions
 def loadData():
     iris = datasets.load_iris()
-    X = iris.data[:,0:2]
+    X = iris.data[:, :2]
     y = iris.target
-    X = X[y != 0, :2]
-    y = y[y != 0]
     return X, y
 
 def splitting_function(X, y):
-    np.random.seed(0)
-    num_sample = len(X)
-    order = np.random.permutation(num_sample)
-    X = X[order]
-    y = y[order].astype(np.float)
-    X_train = X[:int(.5 * num_sample)]
-    y_train = y[:int(.5 * num_sample)]
-    X_validation = X[int(.5 * num_sample):int(.7 * num_sample)]
-    y_validation = y[int(.5 * num_sample):int(.7 * num_sample)]
-    X_test = X[int(.7 * num_sample):]
-    y_test = y[int(.7 * num_sample):]
+    y = y[:].astype(np.float)
+    X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.2, random_state=12)
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.375, random_state=7)
     return X_train, X_validation, X_test, y_train, y_validation, y_test
 
-def svc_function(X, y, kernel, gamma, X_train, y_train, X_validation, y_validation):
-    for fig_num, cost in enumerate((0.001, 0.01, 0.1, 1, 10, 100, 1000)):
-        clf = svm.SVC(kernel=kernel, C=cost, gamma=gamma)
-        clf.fit(X_train, y_train)
+def make_meshgrid(x, y, h=.02):
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),np.arange(y_min, y_max, h))
+    return xx, yy
 
-        plt.figure(fig_num)
-        plt.clf()
-        plt.scatter(X[:, 0], X[:, 1], c=y, zorder=10, cmap=plt.cm.Paired,
-                    edgecolor='k', s=20)
 
-        # Circle out the test data
-        plt.scatter(X_validation[:, 0], X_validation[:, 1], s=80, facecolors='none',
-                    zorder=10, edgecolor='k')
+def plot_contours(clf, xx, yy, **params):
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = plt.contourf(xx, yy, Z, **params)
+    return out
 
-        plt.axis('tight')
-        x_min = X[:, 0].min()
-        x_max = X[:, 0].max()
-        y_min = X[:, 1].min()
-        y_max = X[:, 1].max()
 
-        XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
-        Z = clf.decision_function(np.c_[XX.ravel(), YY.ravel()])
+def svm_with_kernel(X, y, X_train, y_train, X_validation, y_validation, kernel_type, gamma_value):
+    accuracy = []
+    color_map = {-1: (2, 2, 2), 0: (0, 0, .9), 1: (1, 0, 0), 2: (.8, .6, 0)}
+    best_clf = []
+    best_score = 0
+    for i, cost in enumerate((0.001, 0.01, 0.1, 1, 10, 100, 1000)):
+        model = SVC(C=cost, kernel=kernel_type, gamma=gamma_value)
+        clf = model.fit(X_train, y_train)
+        # title for the plots
+        plt.figure(i)
+        title = ('Decision surface using SVM with ' + kernel_type + ' Kernel' )
+        # Set-up grid for plotting.
+        X0, X1 = X_train[:, 0], X_train[:, 1]
+        xx, yy = make_meshgrid(X0, X1)
 
-        # Put the result into a color plot
-        Z = Z.reshape(XX.shape)
-        plt.pcolormesh(XX, YY, Z > 0, cmap=plt.cm.Paired)
-        plt.contour(XX, YY, Z, colors=['k', 'k', 'k'],
-                    linestyles=['--', '-', '--'], levels=[-.5, 0, .5])
-
-        plt.title(str.capitalize(kernel) + ' SVM with C = ' + str(cost) + '\nMean accuracy = ' + str(clf.score(X_validation, y_validation)))
+        plot_contours(clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
+        plt.axis('off')
+        colors = [color_map[y] for y in y_train]
+        plt.scatter(X0, X1, c=y_train, cmap=plt.cm.coolwarm, s=20, edgecolors='black')
+        score = clf.score(X_validation, y_validation)
+        if(score > best_score):
+            best_score = score
+            best_clf = clf
+        accuracy.append((cost,score))
+        plt.suptitle('SVM with C = ' + str(cost) + '\nAccuracy on validation set: ' + str(score))
     plt.show()
+    return accuracy, best_clf
+
+def plotSplineFunction(array, kernel_type):
+    x = array[:,0]
+    y = array[:,1]
+    # print(x)
+    # print(y)
+    xi = [i for i in range(0, len(x))]
+    plt.ylim(0.2,0.9)
+
+    plt.plot(xi, y, marker='o', linestyle='-', color='blue', label='Square')
+    plt.xlabel('C parameter')
+    plt.ylabel('Score')
+    plt.xticks(xi, x)
+
+    #plt.plot (x_new, y)
+    plt.grid(True)
+    plt.title('SVM, {}: C parameter'.format(kernel_type))
+    plt.show()
+    #plt.savefig(rootFolder + "variance_plot" + '.jpg')
 
 #def svc_evaluation(clf, X_test, y_test):
 
 
 X, y = loadData() # gets input and labels arrays
 X_train, X_validation, X_test, y_train, y_validation, y_test = splitting_function(X, y)
-svc_function(X, y, 'linear', 10,  X_train, y_train, X_validation, y_validation)
+
+scores, clf = svm_with_kernel(X, y, X_train, y_train, X_validation, y_validation, 'linear', 'auto')
+print(scores)
+print(clf.score(X_test, y_test))
+plotSplineFunction(np.asarray(scores), 'Linear Kernel')
+
+scores, clf = svm_with_kernel(X, y, X_train, y_train, X_validation, y_validation, 'rbf', 'auto')
+print(scores)
+print(clf.score(X_test, y_test))
+plotSplineFunction(np.asarray(scores), 'RBF Kernel')
